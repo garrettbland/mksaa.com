e1141c758549992d2c82232e6e7244bee929a44d
Garrett Bland <gmorganbland@gmail.com>
2026-03-15 18:15:44 -0500

    feat: init

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+/**
+ * Links in top of website on home page
+ */
+export const ARBITRATION_REQUEST_URL =
+  "https://fs3.formsite.com/yKCdxg/1iuf1mbvo8/index";
+
+export const DEALER_LOGIN_URL = "https://login.mksaa.com/components/login";
+
+/**
+ * Services
+ */
+export const ENTERPRISE_DENT_REPAIR_URL =
+  "https://www.enterprisedentrepair.com/";
+export const FASTBACK_TITLE_URL = "Learn More";
